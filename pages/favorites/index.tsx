1cd4f0262f4c6e6a43392eb338e966d1db35615f
Camilo Valderrama <camilovalderrama@gmail.com>
2022-04-19 19:01:05 -0300

    Primera parte

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components/layouts";
+
+const FavoritesPage = () => {
+    return (
+        <Layout title="Pokómons - Favoritos">
+            <h1>Favorites</h1>
+        </Layout>
+    );
+};
+
+export default FavoritesPage;
