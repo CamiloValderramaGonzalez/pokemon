ec41b7c65e20faccce84c2c0c76e584e2d21d879
Camilo Valderrama <camilovalderrama@gmail.com>
2022-04-23 17:36:45 -0300

    SE agregaron og tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -32,6 +32,45 @@ export const Layout: FC<Props> = ({ children, title }) => {
                     property="og:image"
                     content={`${origin}/img/banner.png`}
                 />
+
+                {/* <!-- MS, fb & Whatsapp -->  */}
+
+                {/* <!-- MS Tile - for Microsoft apps--> */}
+                <meta
+                    name="msapplication-TileImage"
+                    content={`${origin}/img/banner.png`}
+                />
+
+                {/* <!-- fb & Whatsapp --> */}
+
+                {/* <!-- Site Name, Title, and Description to be displayed --> */}
+                <meta property="og:site_name" content="The Rock Photo Studio" />
+                <meta
+                    property="og:title"
+                    content="The Rock Photo Studio in Florida"
+                />
+                <meta
+                    property="og:description"
+                    content="The best photo studio for your events"
+                />
+
+                {/* <!-- Image to display -->*/}
+                {/*<!-- Replace   «example.com/image01.jpg» with your own --> */}
+                <meta
+                    property="og:image"
+                    content={`${origin}/img/banner.png`}
+                />
+
+                {/* <!-- No need to change anything here --> */}
+                <meta property="og:type" content="website" />
+                <meta property="og:image:type" content="image/jpeg" />
+
+                {/* <!-- Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp --> */}
+                <meta property="og:image:width" content="300" />
+                <meta property="og:image:height" content="300" />
+
+                {/* <!-- Website to visit when clicked in fb or WhatsApp--> */}
+                <meta property="og:url" content={`${origin}`} />
             </Head>
             <Navbar />
             <main
